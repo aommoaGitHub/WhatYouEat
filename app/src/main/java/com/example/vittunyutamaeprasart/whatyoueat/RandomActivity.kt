e5bb6dfde60508cf4b7416053ba3af0035433ce1
Vittunyuta Maeprasart <vittunyuta@gmail.com>
2018-05-21 00:50:09 +0700

    create ui of home and random page

--- app/src/main/java/com/example/vittunyutamaeprasart/whatyoueat/RandomActivity.kt
+++ app/src/main/java/com/example/vittunyutamaeprasart/whatyoueat/RandomActivity.kt
@@ -4,27 +4,24 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.content.ClipData.newIntent
 import android.view.WindowManager
 
-
-class MainActivity : AppCompatActivity() {
+class RandomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_random)
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
-    fun goToRandomPage(view: View){
-        val intent = Intent(this, RandomActivity::class.java)
-        startActivity(intent)
-    }
-
     fun goToStorePage(view: View){
         val intent = Intent(this, StoreActivity::class.java)
         startActivity(intent)
+    }
+
+    fun randomAgainButton(view: View){
+
     }
 }
