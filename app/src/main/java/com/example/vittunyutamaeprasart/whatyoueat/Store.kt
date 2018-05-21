9f9e45c244b298ac0ff64b15ea7dccbcc4ab4bf5
Vittunyuta Maeprasart <vittunyuta@gmail.com>
2018-05-22 01:20:20 +0700

    ui initialize success, initialize some classes

---
+++ app/src/main/java/com/example/vittunyutamaeprasart/whatyoueat/Store.kt
@@ -0,0 +1,7 @@
+package com.example.vittunyutamaeprasart.whatyoueat
+
+/**
+ * Created by vittunyutamaeprasart on 22/5/2018 AD.
+ */
+class Store {
+}
---
+++ app/src/main/java/com/example/vittunyutamaeprasart/whatyoueat/StorePresenter.kt
@@ -0,0 +1,8 @@
+package com.example.vittunyutamaeprasart.whatyoueat
+
+/**
+ * Created by vittunyutamaeprasart on 22/5/2018 AD.
+ */
+class StorePresenter{
+
+}
